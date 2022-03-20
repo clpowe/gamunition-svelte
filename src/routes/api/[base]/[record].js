6a8eed014d3e6dd3a129dc9bd2c2caa2a5de8437
clpowe <clpowe@gmail.com>
2022-03-20 01:24:25 -0400

    check env
    aded env check

--- src/routes/api/[base]/[record].js
+++ src/routes/api/[base]/[record].js
@@ -1,5 +1,6 @@
 import Airtable from 'airtable';
 
+const p = process.env.NODE_ENV === 'production' ? process.env : import.meta.env;
 // import { MY_API_KEY } from '$lib/Env';
 
 // let myApiKey;
@@ -15,7 +16,7 @@ import Airtable from 'airtable';
 Airtable.configure({
 	endpointUrl: 'https://api.airtable.com',
 	// @ts-ignore
-	apiKey: import.meta.env.VITE_APIKEY
+	apiKey: p.VITE_APIKEY
 });
 
 var base = Airtable.base('app24nIoWe3Q49B6u');

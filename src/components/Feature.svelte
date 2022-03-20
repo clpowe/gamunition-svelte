d677c8af85eef3f3d21746b78334838fba97f29e
clpowe <clpowe@gmail.com>
2022-03-20 02:14:16 -0400

    static data

--- src/components/Feature.svelte
+++ src/components/Feature.svelte
@@ -10,18 +10,13 @@
 
 <section class="py-40 font-oswald bg-yellow">
 	<div class="text-center max-w-3xl m-auto px-4">
-		{#await record}
-			<LoadingSpinner color="gun-black" />
-		{:then section}
-			<h2 class="text-lg mb-3 md:(text-xl mb-5) leading-none ">
-				{section.record.data.subhead}
-			</h2>
-			<h3 class="text-xl md:(text-2xl) leading-none">{section.record.data.head}</h3>
-			<p class="font-lato text-base leading-5 mt-3 md:(text-lg leading-9 mt-5) ">
-				{section.record.data.text}
-			</p>
-		{:catch error}
-			<p style="color: red">{error.message}</p>
-		{/await}
+		<h2 class="text-lg mb-3 md:(text-xl mb-5) leading-none ">GAMMUNITION ARMS TRAINING, LLC.</h2>
+		<h3 class="text-xl md:(text-2xl) leading-none">
+			ARE YOU A FIRST TIME SHOOTER SHOOTER OR NEW FIREARM OWNER?
+		</h3>
+		<p class="font-lato text-base leading-5 mt-3 md:(text-lg leading-9 mt-5) ">
+			However You’ve Arrive Here, You’re Ready. Allow A Certified NRA Instructor To Transfer The
+			Knowledge, Skills and Attitude You Need To Be A Responsible...
+		</p>
 	</div>
 </section>
